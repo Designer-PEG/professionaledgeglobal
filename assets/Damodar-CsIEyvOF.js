d13acc358918bfe39c576d38357c05dbdd16ca7f
pujanjoci <pujanjoci2@gmail.com>
2026-06-27 10:27:57 +0545

    Updates

--- assets/Damodar-CsIEyvOF.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/Suresh-_f9DcuCo.jpg",a="/assets/Damodar-B2onKirS.jpg";export{a as D,s as S};
---
+++ assets/Damodar-DGXtsYri.js
@@ -0,0 +1,1 @@
+const s="/assets/Suresh-GyTWIPaI.webp",a="/assets/Damodar-EuYyX8h8.webp";export{a as D,s as S};
